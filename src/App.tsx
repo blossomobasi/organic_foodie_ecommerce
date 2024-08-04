bb118231b3d7e35ade2f73de68479e4f25d8faa1
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-04 22:32:31 +0100

    Add router handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AppLayout from "./components/AppLayout";
+import HomePage from "./pages/HomePage";
+import ProductsPage from "./pages/ProductsPage";
+import HealthBenefitsPage from "./pages/HealthBenefitsPage";
+import BlogsPage from "./pages/BlogsPage";
+import FaqPage from "./pages/FaqPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
 function App() {
-    return <div>App</div>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<AppLayout />}>
+                    <Route index element={<HomePage />} />
+                    <Route path="products" element={<ProductsPage />} />
+                    <Route path="health-benefits" element={<HealthBenefitsPage />} />
+                    <Route path="blogs" element={<BlogsPage />} />
+                    <Route path="faq" element={<FaqPage />} />
+                </Route>
+
+                <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
