31c4c5328dd1ebe711df2a9ca0805cb980561a04
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-05 08:32:55 +0100

    Add image asset

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,50 @@
+import { Link } from "react-router-dom";
+import Logo from "./Logo";
+import { FiHeart } from "react-icons/fi";
+import { BsHandbag } from "react-icons/bs";
+import Button from "../ui/Button";
+import { SlMagnifier } from "react-icons/sl";
+
 const NavBar = () => {
-    return <div>NavBar</div>;
+    const navlinks = [
+        { name: "Home", url: "/" },
+        { name: "Our Products", url: "products" },
+        { name: "Health Benefits", url: "health-benefits" },
+        { name: "Blogs", url: "blogs" },
+        { name: "FAQs", url: "faq" },
+    ];
+
+    return (
+        <header>
+            <div className="flex items-center justify-between px-[5rem] py-3 max-w-[120rem]">
+                <span className="flex-[0.5]">
+                    <Logo />
+                </span>
+
+                <nav className="flex-[1.8]">
+                    <ul className="flex justify-between">
+                        {navlinks.map((link) => (
+                            <li key={link.name} className="hover:font-bold">
+                                <Link to={link.url}>{link.name}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+
+                <div className="flex-[0.5]">
+                    <span className="border-x border-grey-200 flex items-center w-fit justify-center mx-auto px-2">
+                        <SlMagnifier size={25} />
+                    </span>
+                </div>
+
+                <span className="flex items-center gap-x-4">
+                    <FiHeart size={25} />
+                    <BsHandbag size={25} />
+                    <Button>Contact Us</Button>
+                </span>
+            </div>
+        </header>
+    );
 };
 
 export default NavBar;
