8f0cd3bed247ea443e4184f2a57c72460f2a48cd
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-05 08:33:46 +0100

    Create a logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,11 @@
+import OrganicLogo from "../assets/images/organic_foodie_logo.png";
+
+const Logo = () => {
+    return (
+        <div>
+            <img src={OrganicLogo} alt="Organic Foodie Logo" />
+        </div>
+    );
+};
+
+export default Logo;
