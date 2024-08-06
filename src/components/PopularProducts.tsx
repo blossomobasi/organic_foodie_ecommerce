486d7eab908a27dadc688dec8de451695be0b251
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-06 14:20:45 +0100

    Update: Use DisplayProducts to display popular products

--- src/components/PopularProducts.tsx
+++ src/components/PopularProducts.tsx
@@ -1,58 +1,20 @@
 import { popularProductsData } from "../data/productsData";
 import Button from "../ui/Button";
-import { IoStarSharp } from "react-icons/io5";
-import { FiHeart } from "react-icons/fi";
 
 import OrganicSnacks from "../assets/images/organic_snacks.png";
+import DisplayProducts from "../ui/DisplayProducts";
 
 const PopularProducts = () => {
     const popularProducts = popularProductsData;
     return (
-        // <section className="px-[5rem] rounded-t-[3.5rem] absolute -translate-y-14 bg-white w-full pt-24">
         <section className="px-[5rem] rounded-t-[3.5rem] bg-white w-full pt-24 -translate-y-14">
             <div>
-                <div>
-                    <h2 className="text-5xl font-bold text-black">Our Popular Products</h2>
-                    <div className="flex justify-between items-center py-5">
-                        <p className="text-grey-600">
-                            Browse our most popular snacks and make your day <br /> more beautiful
-                            and glorious.
-                        </p>
-                        <Button url="products" variant="primary-outline">
-                            Browse All
-                        </Button>
-                    </div>
-                </div>
-
-                <div className="flex space-x-6 overflow-x-auto pb-8">
-                    {popularProducts.map((product) => (
-                        <div
-                            key={product.title + crypto.randomUUID()}
-                            className="w-[23.5rem] flex-shrink-0"
-                        >
-                            <img src={product.image} alt={product.title} className="w-full" />
-                            <p className="flex justify-between items-center py-3">
-                                <span className="text-grey-600">{product.category}</span>
-                                <span>
-                                    <FiHeart size={20} />
-                                </span>
-                            </p>
-                            <h4 className="font-semibold text-lg">{product.title}</h4>
-                            <p className="flex items-center justify-between py-3">
-                                <span className="flex items-center space-x-2 text-grey-600">
-                                    <IoStarSharp size={20} className="text-secondaryOrange-400" />
-                                    <span>
-                                        {product.totalReview}({product.averageReview})
-                                    </span>
-                                </span>
-                                <span className="font-medium">${product.price}</span>
-                            </p>
-                            <Button variant="primary-outline" className="w-full mt-3">
-                                Add to Cart
-                            </Button>
-                        </div>
-                    ))}
-                </div>
+                <DisplayProducts
+                    data={popularProducts}
+                    description=" Browse our most popular snacks and make your day <br /> more
+                                beautiful and glorious."
+                    title="Our Popular Products"
+                />
 
                 <div className="flex items-center justify-between py-20">
                     <div className="w-full flex flex-col items-center space-y-5">
