587ace558262aae85e028f3397cd869e6221eebd
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-06 11:54:14 +0100

    Add new style

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 import OrganicLogo from "../assets/images/organic_foodie_logo.png";
 
 const Logo = () => {
     return (
-        <div>
-            <img src={OrganicLogo} alt="Organic Foodie Logo" />
+        <div className="w-fit">
+            <Link to="/">
+                <img src={OrganicLogo} alt="Organic Foodie Logo" />
+            </Link>
         </div>
     );
 };
