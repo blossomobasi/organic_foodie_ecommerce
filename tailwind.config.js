950014ef0393887b272b55b631a5509d7bf7c994
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-07 12:36:24 +0100

    Add tailwind scrollbar as a plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindScrollbar from "tailwind-scrollbar";
+
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
@@ -30,5 +32,5 @@ export default {
             },
         },
     },
-    plugins: [],
+    plugins: [tailwindScrollbar],
 };
