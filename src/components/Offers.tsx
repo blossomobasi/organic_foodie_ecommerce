aec57ac1462d038e3644c8361e2c30bc9a7eb91c
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-08 17:58:11 +0100

    Add responsiveness for very large screens

--- src/components/Offers.tsx
+++ src/components/Offers.tsx
@@ -48,57 +48,59 @@ const Offers = () => {
     const mobileView = windowWidth < 500;
 
     return (
-        <section className="lg:px-20 px-5 pb-20">
-            <div className="flex justify-between items-end space-x-5 pb-10">
-                <h3 className="md:text-4xl text-3xl font-bold nichrome">
-                    Hurry Do not Miss Out On {mobileView ? " " : <br />} This Offers
-                </h3>
-                <Button className="rounded-none whitespace-nowrap">Browse All</Button>
-            </div>
+        <section className="lg:px-20 px-5 pb-20 flex flex-col items-center">
+            <div className="max-w-[110rem] w-full">
+                <div className="flex justify-between items-end space-x-5 pb-10">
+                    <h3 className="md:text-4xl text-3xl font-bold nichrome">
+                        Hurry Do not Miss Out On {mobileView ? " " : <br />} This Offers
+                    </h3>
+                    <Button className="rounded-none whitespace-nowrap">Browse All</Button>
+                </div>
 
-            <div className="grid md:grid-cols-3 grid-cols-2 gap-5">
-                <img
-                    src={BlackFridaySales}
-                    alt="Black Friday Sales"
-                    className="h-full w-full col-span-2"
-                />
-                {blackFriday.map((product) => (
-                    <div key={product.title + crypto.randomUUID()} className="w-full">
-                        <div className="relative w-full">
-                            <img src={product.image} alt={product.title} className="w-full" />
+                <div className="grid md:grid-cols-3 grid-cols-2 gap-5">
+                    <img
+                        src={BlackFridaySales}
+                        alt="Black Friday Sales"
+                        className="h-full w-full col-span-2"
+                    />
+                    {blackFriday.map((product) => (
+                        <div key={product.title + crypto.randomUUID()} className="w-full">
+                            <div className="relative w-full">
+                                <img src={product.image} alt={product.title} className="w-full" />
 
-                            {product.stock && (
-                                <div className="bg-secondaryOrange-500 text-white absolute top-1/2 -translate-y-1/2 py-2 text-center font-semibold w-full uppercase">
-                                    Out Of Stock
-                                </div>
-                            )}
-                        </div>
+                                {product.stock && (
+                                    <div className="bg-secondaryOrange-500 text-white absolute top-1/2 -translate-y-1/2 py-2 text-center font-semibold w-full uppercase">
+                                        Out Of Stock
+                                    </div>
+                                )}
+                            </div>
 
-                        <p className="flex justify-between items-center py-3">
-                            <span className="text-grey-600 md:text-base text-sm">
-                                {product.category}
-                            </span>
-                            <span>
-                                <FiHeart size={!mobileView ? 20 : ""} />
-                            </span>
-                        </p>
-                        <h4 className="font-semibold md:text-xl md:tracking-normal tracking-wide nichrome">
-                            {product.title}
-                        </h4>
-                        <p className="flex items-center justify-between py-3 md:text-base text-sm">
-                            <span className="flex items-center space-x-2 text-grey-600">
-                                <IoStarSharp size={20} className="text-secondaryOrange-400" />
-                                <span>
-                                    {product.totalReview}({product.averageReview})
+                            <p className="flex justify-between items-center py-3">
+                                <span className="text-grey-600 md:text-base text-sm">
+                                    {product.category}
                                 </span>
-                            </span>
-                            <span className="font-medium">${product.price}</span>
-                        </p>
-                        <Button variant="primary-outline" className="w-full mt-3 pt-1.5">
-                            Add to Cart
-                        </Button>
-                    </div>
-                ))}
+                                <span>
+                                    <FiHeart size={!mobileView ? 20 : ""} />
+                                </span>
+                            </p>
+                            <h4 className="font-semibold md:text-xl md:tracking-normal tracking-wide nichrome">
+                                {product.title}
+                            </h4>
+                            <p className="flex items-center justify-between py-3 md:text-base text-sm">
+                                <span className="flex items-center space-x-2 text-grey-600">
+                                    <IoStarSharp size={20} className="text-secondaryOrange-400" />
+                                    <span>
+                                        {product.totalReview}({product.averageReview})
+                                    </span>
+                                </span>
+                                <span className="font-medium">${product.price}</span>
+                            </p>
+                            <Button variant="primary-outline" className="w-full mt-3 pt-1.5">
+                                Add to Cart
+                            </Button>
+                        </div>
+                    ))}
+                </div>
             </div>
         </section>
     );
