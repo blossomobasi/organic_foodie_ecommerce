bda18a047dd82b8b629db3438859f40bb8bc09d4
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-12 10:58:08 +0100

    Add similarProductData

--- src/data/productsData.ts
+++ src/data/productsData.ts
@@ -104,3 +104,54 @@ export const newProductsData: ProductsType[] = [
         averageReview: 5,
     },
 ];
+
+export const similarProductsData: ProductsType[] = [
+    {
+        image: OrganicAlmondDelight,
+        title: "Organic Almond Delight",
+        category: "Coconut Flakes",
+        price: 110,
+        totalReview: 18,
+        averageReview: 5,
+    },
+    {
+        image: BerryBlissBites,
+        title: "Berry Bliss Bites",
+        category: "Coconut Flakes",
+        price: 139,
+        totalReview: 28,
+        averageReview: 5,
+    },
+    {
+        image: CoconutCrunchies,
+        title: "Coconut Crunchies",
+        category: "Coconut Flakes",
+        price: 399,
+        totalReview: 102,
+        averageReview: 5,
+    },
+    {
+        image: OrganicAlmondDelight,
+        title: "Organic Almond Delight",
+        category: "Coconut Flakes",
+        price: 110,
+        totalReview: 18,
+        averageReview: 5,
+    },
+    {
+        image: BerryBlissBites,
+        title: "Berry Bliss Bites",
+        category: "Coconut Flakes",
+        price: 139,
+        totalReview: 28,
+        averageReview: 5,
+    },
+    {
+        image: CoconutCrunchies,
+        title: "Coconut Crunchies",
+        category: "Coconut Flakes",
+        price: 399,
+        totalReview: 102,
+        averageReview: 5,
+    },
+];
