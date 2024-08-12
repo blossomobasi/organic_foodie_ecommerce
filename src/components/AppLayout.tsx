f8dc7049c336340a462735b5d27b27abb7d49c0c
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-12 14:22:44 +0100

    Remove unnecessary code

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,13 +1,8 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
-import { useEffect } from "react";
 
 const AppLayout = () => {
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
-
     return (
         <div>
             <NavBar />
