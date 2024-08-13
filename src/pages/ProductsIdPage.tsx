bc6e09dff398076477a54dcc75e3d151b695b237
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-13 19:18:57 +0100

    Add similar products

--- src/pages/ProductsIdPage.tsx
+++ src/pages/ProductsIdPage.tsx
@@ -1,9 +1,18 @@
 import { useParams } from "react-router-dom";
+import SimilarProduct from "../components/SimilarProduct";
 
 const ProductsIdPage = () => {
     const params = useParams();
 
-    return <div className="text-2xl text-center py-5">ID: {params.productId}</div>;
+    return (
+        <section className="lg:px-20 px-5 flex flex-col items-center">
+            <div className="max-w-[110rem] w-full">
+                <div className="text-center text-2xl font-bold py-5">ID: {params.productId}</div>
+
+                <SimilarProduct />
+            </div>
+        </section>
+    );
 };
 
 export default ProductsIdPage;
