13090132c7498b5a94186ec3e840dd67a8caf331
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-21 19:32:31 +0100

    Add more button props

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,6 +1,7 @@
 import { cn } from "../utils/cn";
 import { cva } from "class-variance-authority";
 import { useNavigate } from "react-router-dom";
+import MiniSpinner from "./MiniSpinner";
 
 type ButtonProps = React.HTMLAttributes<HTMLButtonElement> & {
     children: React.ReactNode;
@@ -8,9 +9,19 @@ type ButtonProps = React.HTMLAttributes<HTMLButtonElement> & {
     icon?: React.ReactNode;
     url?: string;
     onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+    isLoading?: boolean;
 };
 
-const Button = ({ children, variant, icon, className, url, onClick, ...props }: ButtonProps) => {
+const Button = ({
+    children,
+    variant,
+    icon,
+    className,
+    url,
+    onClick,
+    isLoading,
+    ...props
+}: ButtonProps) => {
     const navigate = useNavigate();
 
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -27,8 +38,15 @@ const Button = ({ children, variant, icon, className, url, onClick, ...props }: 
             {...props}
             className={cn(buttonVariant({ variant }), className)}
             onClick={handleClick}
+            disabled={isLoading}
         >
-            {children}
+            {isLoading ? (
+                <span className="flex items-center justify-center">
+                    <MiniSpinner />
+                </span>
+            ) : (
+                children
+            )}
             {icon && <span>{icon}</span>}
         </button>
     );
@@ -38,11 +56,11 @@ const buttonVariant = cva("py-2.5 px-6", {
     variants: {
         variant: {
             primary:
-                // "border border-primaryGreen-700 text-white bg-primaryGreen-700 rounded-md font-semibold hover:bg-primaryGreen-300 ring-offset-2 focus:ring ring-primaryGreen-300",
-                "border border-primaryGreen-700 hover:border-secondaryOrange-400 text-white bg-primaryGreen-700 rounded-md font-semibold hover:bg-secondaryOrange-400 ring-offset-2 focus:ring ring-primaryGreen-300",
+                "border border-primaryGreen-700 text-white bg-primaryGreen-700 rounded-md font-semibold hover:bg-primaryGreen-300 ring-offset-2 focus:ring ring-primaryGreen-300",
+            // "border border-primaryGreen-700 hover:border-secondaryOrange-400 text-white bg-primaryGreen-700 rounded-md font-semibold hover:bg-secondaryOrange-400 ring-offset-2 focus:ring ring-primaryGreen-300",
             secondary:
-                // "bg-secondaryOrange-400 hover:bg-secondaryOrange-500 ring-offset-2 focus:ring ring-secondaryOrange-400 text-white rounded-md border border-secondaryOrange-400 font-semibold",
-                "bg-secondaryOrange-400 hover:bg-primaryGreen-700 ring-offset-2 focus:ring ring-secondaryOrange-400 text-white rounded-md border border-secondaryOrange-400 hover:border-primaryGreen-700 font-semibold",
+                "bg-secondaryOrange-400 hover:bg-secondaryOrange-500 ring-offset-2 focus:ring ring-secondaryOrange-400 text-white rounded-md border border-secondaryOrange-400 font-semibold",
+            // "bg-secondaryOrange-400 hover:bg-primaryGreen-700 ring-offset-2 focus:ring ring-secondaryOrange-400 text-white rounded-md border border-secondaryOrange-400 hover:border-primaryGreen-700 font-semibold",
             "primary-outline":
                 "border border-primaryGreen-700 rounded-md text-primaryGreen-700 font-semibold hover:bg-primaryGreen-100",
         },
