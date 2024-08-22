6abbce375d7f1fb804a2c5f79735e70acf92e208
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-22 13:10:04 +0100

    Use real data

--- src/components/SimilarProduct.tsx
+++ src/components/SimilarProduct.tsx
@@ -1,14 +1,16 @@
-import { similarProductsData } from "../data/productsData";
+// import { similarProductsData } from "../data/productsData";
+import { useProducts } from "../hooks/useProduct";
 import DisplayProducts from "../ui/DisplayProducts";
 
 const SimilarProduct = () => {
-    const similarProducts = similarProductsData;
+    // const similarProducts = similarProductsData;
+    const { products } = useProducts();
 
     return (
         <section className="bg-white w-full pt-10 pb-20 flex flex-col items-center">
             <div className="max-w-[110rem] w-full">
                 <DisplayProducts
-                    data={similarProducts}
+                    data={products?.allProduct}
                     title="People Also Buy"
                     description="Browse our most popular snacks and make your day more beautiful and glorious"
                 />
