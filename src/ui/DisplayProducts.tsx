2e8dca1586221a51527550aa5633ae3d7c39df35
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-22 12:38:26 +0100

    Use real data and types

--- src/ui/DisplayProducts.tsx
+++ src/ui/DisplayProducts.tsx
@@ -1,16 +1,24 @@
-import { FiHeart } from "react-icons/fi";
-import Button from "./Button";
-import { IoStarSharp } from "react-icons/io5";
-import { ProductsType } from "../types/products";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import { FiHeart } from "react-icons/fi";
+import { IoStarSharp } from "react-icons/io5";
+
+import { Product } from "../types/products";
+
+import Button from "./Button";
+import { useProducts } from "../hooks/useProduct";
+import Spinner from "./Spinner";
 
 type Props = {
-    data: ProductsType[];
+    data: Product[] | undefined;
     title: string;
     description: string;
 };
 
 const DisplayProducts = ({ data, title, description }: Props) => {
+    const { isLoading } = useProducts();
+    const navigate = useNavigate();
     const windowWidth = window.innerWidth;
     const mobileView = windowWidth < 500;
 
@@ -38,35 +46,44 @@ const DisplayProducts = ({ data, title, description }: Props) => {
                 </div>
             </div>
 
-            <div className="flex space-x-6 overflow-x-auto pb-8 scrollbar-thin scrollbar-thumb-grey-900 scrollbar-track-grey-500">
-                {data.map((product) => (
-                    <div
-                        key={product.title + crypto.randomUUID()}
-                        className="w-[23.5rem] flex-shrink-0"
-                    >
-                        <img src={product.image} alt={product.title} className="w-full" />
-                        <p className="flex justify-between items-center py-3">
-                            <span className="text-grey-600">{product.category}</span>
-                            <span>
-                                <FiHeart size={20} />
-                            </span>
-                        </p>
-                        <h4 className="font-semibold text-xl nichrome">{product.title}</h4>
-                        <p className="flex items-center justify-between py-3">
-                            <span className="flex items-center space-x-2 text-grey-600">
-                                <IoStarSharp size={20} className="text-secondaryOrange-400" />
+            {isLoading ? (
+                <Spinner />
+            ) : (
+                <div className="flex space-x-6 overflow-x-auto pb-8 scrollbar-thin scrollbar-thumb-grey-900 scrollbar-track-grey-500">
+                    {data?.map((product) => (
+                        <div
+                            key={product.title + crypto.randomUUID()}
+                            className="w-[23.5rem] flex-shrink-0 cursor-pointer"
+                            onClick={() => navigate(`/products/${product._id}`)}
+                        >
+                            <img
+                                src={"https://i.ytimg.com/vi/S5TNp4v5s14/maxresdefault.jpg"}
+                                alt={product.title}
+                                className="w-full"
+                            />
+                            <p className="flex justify-between items-center py-3">
+                                <span className="text-grey-600">{product.category}</span>
                                 <span>
-                                    {product.totalReview}({product.averageReview})
+                                    <FiHeart size={20} />
                                 </span>
-                            </span>
-                            <span className="font-medium">${product.price}</span>
-                        </p>
-                        <Button variant="primary-outline" className="w-full mt-3">
-                            Add to Cart
-                        </Button>
-                    </div>
-                ))}
-            </div>
+                            </p>
+                            <h4 className="font-semibold text-xl nichrome capitalize">
+                                {product.title}
+                            </h4>
+                            <p className="flex items-center justify-between py-3">
+                                <span className="flex items-center space-x-2 text-grey-600">
+                                    <IoStarSharp size={20} className="text-secondaryOrange-400" />
+                                    <span>{product.totalRating}(5)</span>
+                                </span>
+                                <span className="font-medium">${product.price}</span>
+                            </p>
+                            <Button variant="primary-outline" className="w-full mt-3">
+                                Add to Cart
+                            </Button>
+                        </div>
+                    ))}
+                </div>
+            )}
         </React.Fragment>
     );
 };
