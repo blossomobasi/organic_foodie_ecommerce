0a267184fd2c38e0c2ec6ae8c2688f8f0be13a33
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-22 13:09:54 +0100

    User real data

--- src/components/NewProducts.tsx
+++ src/components/NewProducts.tsx
@@ -1,14 +1,16 @@
-import { newProductsData } from "../data/productsData";
+// import { newProductsData } from "../data/productsData";
+import { useProducts } from "../hooks/useProduct";
 import DisplayProducts from "../ui/DisplayProducts";
 
 const NewProducts = () => {
-    const newProducts = newProductsData;
+    // const newProducts = newProductsData;
+    const { products } = useProducts();
 
     return (
         <section className="lg:px-20 px-5 bg-white w-full pt-10 pb-20 flex flex-col items-center">
             <div className="max-w-[110rem] w-full">
                 <DisplayProducts
-                    data={newProducts}
+                    data={products?.allProduct}
                     title="Our New Products"
                     description="Browse our most popular snacks and make your day <br /> more beautiful
                             and glorious"
