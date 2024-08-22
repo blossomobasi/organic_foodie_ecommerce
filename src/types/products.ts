564d19cc7cee47606485bb49050dee4831b51772
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-22 12:38:08 +0100

    Add product and rating types

--- src/types/products.ts
+++ src/types/products.ts
@@ -6,3 +6,36 @@ export type ProductsType = {
     totalReview: number;
     averageReview: number;
 };
+
+export type Product = {
+    _id: string;
+    title: string;
+    description: string;
+    price: number;
+    category: string;
+    brand: string;
+    quantity: string;
+    sold: string;
+    images: string[];
+    totalRating: number;
+    rating: Rating[];
+};
+
+export type Rating = {
+    _id: string;
+    star: number;
+    comment: string;
+    postedBy: string;
+};
+
+export type ProductResponse = {
+    success: boolean;
+    message: string;
+    allProduct: Product[];
+};
+
+export type SingleProductResponse = {
+    success: boolean;
+    message: string;
+    singleProduct: Product;
+};
