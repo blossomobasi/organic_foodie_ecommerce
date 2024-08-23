b79b42f04a848db1e521a0c91e08b240078cb740
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-23 07:23:02 +0100

    Create a scroll to top component

---
+++ src/ui/ScrollToTop.tsx
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+
+const ScrollToTop = ({ children }: { children: React.ReactNode }) => {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+    return <>{children}</>;
+};
+
+export default ScrollToTop;
