f7b550315c3a9bd12f088caf116c38501187aed8
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-23 07:50:50 +0100

    Use new products api

--- src/components/NewProducts.tsx
+++ src/components/NewProducts.tsx
@@ -1,16 +1,14 @@
-// import { newProductsData } from "../data/productsData";
 import { useProducts } from "../hooks/useProduct";
 import DisplayProducts from "../ui/DisplayProducts";
 
 const NewProducts = () => {
-    // const newProducts = newProductsData;
-    const { products } = useProducts();
+    const { newProducts } = useProducts();
 
     return (
         <section className="lg:px-20 px-5 bg-white w-full pt-10 pb-20 flex flex-col items-center">
             <div className="max-w-[110rem] w-full">
                 <DisplayProducts
-                    data={products?.allProduct}
+                    data={newProducts?.products}
                     title="Our New Products"
                     description="Browse our most popular snacks and make your day <br /> more beautiful
                             and glorious"
