705c3150c520706ad548da1dfe7ca8e37140cbf3
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-26 12:09:04 +0100

    Adding padding in the y axis

--- src/components/EmptyCart.tsx
+++ src/components/EmptyCart.tsx
@@ -1,6 +1,6 @@
 const EmptyCart = () => {
     return (
-        <span className="flex flex-col items-center justify-center space-y-2 w-full">
+        <span className="flex flex-col items-center justify-center space-y-2 w-full py-20">
             <h2 className="text-5xl font-bold nichrome">There is no Item in your cart :(</h2>
             <p className="text-grey-600">Make your cart happy by adding an item :)</p>
         </span>
