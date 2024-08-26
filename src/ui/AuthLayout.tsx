e5365aa4ff95deb1a82ff9dcc4889c1c281d61dc
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-26 14:09:07 +0100

    Create Auth layout

---
+++ src/ui/AuthLayout.tsx
@@ -0,0 +1,21 @@
+import AuthImage from "../assets/images/auth_image.png";
+import Logo from "../components/Logo";
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className="flex md:flex-row flex-col md:gap-y-0 gap-y-5 md:h-screen">
+            <div className="md:w-1/2 md:h-full h-[20rem] relative">
+                <img src={AuthImage} alt="Auth Image" className="h-full w-full object-cover" />
+                <div className="absolute top-[10%] left-1/2 -translate-x-1/2">
+                    <Logo />
+                </div>
+            </div>
+
+            <div className="md:w-1/2 lg:p-10 p-5 h-full overflow-y-auto flex justify-center">
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default AuthLayout;
