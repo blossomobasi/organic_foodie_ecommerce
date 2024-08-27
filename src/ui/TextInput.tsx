3d789783ab37135365803b178ed700bf8e048a84
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-27 11:14:47 +0100

    Handle show password implementation

--- src/ui/TextInput.tsx
+++ src/ui/TextInput.tsx
@@ -1,5 +1,7 @@
 import React, { ForwardedRef } from "react";
 import { cn } from "../utils/cn";
+import { FaEyeSlash } from "react-icons/fa";
+import { IoEyeSharp } from "react-icons/io5";
 
 type Props = {
     type?: string;
@@ -9,11 +11,24 @@ type Props = {
     inputId?: string;
     className?: string;
     authInput?: boolean;
+    onIconClick?: () => void;
+    showPassword?: boolean;
 };
 
 const TextInput = React.forwardRef<HTMLInputElement, Props>(
     (
-        { label, error, placeholder, inputId, type = "text", className, authInput, ...rest },
+        {
+            label,
+            error,
+            placeholder,
+            inputId,
+            type = "text",
+            className,
+            authInput,
+            onIconClick,
+            showPassword,
+            ...rest
+        },
         ref: ForwardedRef<HTMLInputElement>
     ) => (
         <div className="flex flex-col space-y-2.5">
@@ -37,19 +52,40 @@ const TextInput = React.forwardRef<HTMLInputElement, Props>(
                     placeholder={placeholder}
                 />
             ) : (
-                <input
-                    {...rest}
-                    ref={ref}
-                    type={type}
-                    id={inputId}
-                    className={cn(
-                        "border-b py-3 w-full placeholder:text-[#566363] text-[#566363] focus-within:outline-none",
-                        className,
-                        error ? "border-secondaryOrange-500" : "border-grey-400",
-                        className
+                <span className="flex items-center relative">
+                    <input
+                        {...rest}
+                        ref={ref}
+                        type={type}
+                        id={inputId}
+                        className={cn(
+                            "border-b py-3 w-full placeholder:text-[#566363] text-[#566363] focus-within:outline-none",
+                            className,
+                            error ? "border-secondaryOrange-500" : "border-grey-400",
+                            className
+                        )}
+                        placeholder={placeholder}
+                    />
+
+                    {type === "password" && !showPassword ? (
+                        <span
+                            className="absolute right-2 cursor-pointer text-[#566363]"
+                            onClick={onIconClick}
+                        >
+                            <FaEyeSlash size={23} />
+                        </span>
+                    ) : (
+                        type === "text" &&
+                        showPassword && (
+                            <span
+                                className="absolute right-2 cursor-pointer text-[#566363]"
+                                onClick={onIconClick}
+                            >
+                                <IoEyeSharp size={23} />
+                            </span>
+                        )
                     )}
-                    placeholder={placeholder}
-                />
+                </span>
             )}
             {error && <span className="text-secondaryOrange-500">{error}</span>}
         </div>
