87f40634392fa2cf031b5293639e29f635239fae
Obasi Chizitere <blossomobasi2@gmail.com>
2024-09-10 10:17:23 +0100

    Update cart response

--- src/types/cart.ts
+++ src/types/cart.ts
@@ -2,8 +2,25 @@ export type CartData = {
     itemId: string;
 };
 
+export type Cart = {
+    _id: string;
+    cartTotal: string;
+    createdAt: string;
+    orderBy: string;
+    orderStatus: string;
+    __v: number
+    products: {
+        _id: string;
+        count: string;
+        date: string;
+        price: number;
+        productId: string
+    }[]
+}
+
+
 export type CartResponse = {
     success: boolean;
     message: string;
-    cartData: string[];
+    userOrdersCart: Cart[]
 };
