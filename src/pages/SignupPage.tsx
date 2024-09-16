79428dd5d4930f2687bb7018292759b0522c9bfa
Obasi Chizitere <blossomobasi2@gmail.com>
2024-09-16 07:27:33 +0100

    Add semi-colon

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -24,7 +24,7 @@ const SignupPage = () => {
             navigate("/login");
         },
         onError: (err: AxiosError) => {
-            const errorMessage = (err.response?.data as { message: string}).message
+            const errorMessage = (err.response?.data as { message: string }).message;
             toast.error(errorMessage);
         },
     });
