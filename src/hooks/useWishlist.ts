fe7d270c40bda4bcb6d6ab23b495979549498d85
Obasi Chizitere <blossomobasi2@gmail.com>
2024-09-16 12:02:31 +0100

    Add hook for getting wishlilst

--- src/hooks/useWishlist.ts
+++ src/hooks/useWishlist.ts
@@ -1,10 +1,19 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { addToWishlist as addToWishlistApi } from "../services";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { addToWishlist as addToWishlistApi, getAllProductsInWishlist } from "../services";
 import { toast } from "react-toastify";
 import { AxiosError } from "axios";
 
-const useWishlist = () => {
+const useWishlist = (userId?: string) => {
     const queryClient = useQueryClient();
+    const {
+        data: wishlist,
+        isLoading: isLoadingWishlist,
+        error,
+    } = useQuery({
+        queryKey: ["products", userId],
+        queryFn: () => getAllProductsInWishlist(userId as string),
+    });
+
     const { mutate: addToWishlist, isPending: isAddingToWishlist } = useMutation({
         mutationFn: ({ prodId, userId }: { prodId: string; userId: string }) =>
             addToWishlistApi(prodId, userId),
@@ -20,7 +29,7 @@ const useWishlist = () => {
         },
     });
 
-    return { addToWishlist, isAddingToWishlist };
+    return { wishlist, isLoadingWishlist, error, addToWishlist, isAddingToWishlist };
 };
 
 export default useWishlist;
