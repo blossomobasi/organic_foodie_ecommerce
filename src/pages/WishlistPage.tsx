d9a418fc68f014fb8ae339d92f572e3d8a412c3c
Obasi Chizitere <blossomobasi2@gmail.com>
2024-09-16 12:02:47 +0100

    Remove fake data and use real data

--- src/pages/WishlistPage.tsx
+++ src/pages/WishlistPage.tsx
@@ -1,36 +1,34 @@
-import OrganicSnacks from "../assets/images/black_friday_out_of_stock.png";
-import BerryBlissBites from "../assets/images/berry_bliss_bites.png";
-import CoconutCrunchies from "../assets/images/crispy_coconut_crunchies.png";
+import Cookies from "js-cookie";
 import Button from "../ui/Button";
 import { LiaTimesSolid } from "react-icons/lia";
 import ScrollToTop from "../ui/ScrollToTop";
+import useWishlist from "../hooks/useWishlist";
+import { useAddToCart } from "../hooks/useCart";
 
 const WishlistPage = () => {
-    const wishlists = [
-        {
-            image: OrganicSnacks,
-            title: "Organic Almond Delight",
-            category: "Coconut Flakes",
-            unitPrice: 237.43,
-            stock: "In Stock",
-        },
-        {
-            image: BerryBlissBites,
-            title: "Seed Power Bars",
-            category: "Coconut Flakes",
-            unitPrice: 874.45,
-            stock: "In Stock",
-        },
-        {
-            image: CoconutCrunchies,
-            title: "Coconut Date Energy Bars",
-            category: "Coconut Flakes",
-            unitPrice: 237.43,
-            stock: "In Stock",
-        },
-    ];
-    const totalWishlist = wishlists.length;
-    const totalPrice = wishlists.reduce((acc, item) => acc + item.unitPrice, 0);
+    const userId = Cookies.get("userId") || "";
+    const {
+        wishlist,
+        isLoadingWishlist,
+        error,
+        addToWishlist: removeFromWishlist,
+    } = useWishlist(userId);
+    const { addToCart, isPending } = useAddToCart();
+
+    if (isLoadingWishlist) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
+
+    const WISHLIST_LENGTH = wishlist?.products.length;
+    const TOTAL_PRICE = wishlist?.products?.reduce((acc, item) => acc + item.price, 0);
+
+    function handleAddToCart(productId: string) {
+        if (isPending) return;
+        addToCart({ productId, count: 1, userId });
+    }
+    function handleRemoveWishlistItem(prodId: string) {
+        if (isPending) return;
+        removeFromWishlist({ prodId, userId });
+    }
 
     return (
         <ScrollToTop>
@@ -38,7 +36,8 @@ const WishlistPage = () => {
                 <div className="md:py-14 py-5 text-center lg:px-20 px-5 max-w-[120rem] w-full">
                     <h2 className="text-5xl font-bold nichrome md:pb-5 pb-3">Wish List</h2>
                     <p className="md:mb-10 mb-7 text-grey-600">
-                        {totalWishlist} items in your wishlist
+                        {WISHLIST_LENGTH} {Number(WISHLIST_LENGTH) > 1 ? "items" : "item"} in your
+                        wishlist
                     </p>
 
                     <div className=" overflow-x-auto">
@@ -53,25 +52,36 @@ const WishlistPage = () => {
                             </thead>
 
                             <tbody>
-                                {wishlists.map((wishlist, index) => (
+                                {wishlist?.products?.map((product, index) => (
                                     <tr key={index} className="border">
                                         <td className="p-5 flex items-center space-x-5">
-                                            <span className="cursor-pointer">
+                                            <span
+                                                className="cursor-pointer"
+                                                onClick={() =>
+                                                    handleRemoveWishlistItem(product._id)
+                                                }
+                                            >
                                                 <LiaTimesSolid size={25} />
                                             </span>
                                             <img
-                                                src={wishlist.image}
-                                                alt={wishlist.title}
+                                                src={product.images[0]}
+                                                alt={product.title}
                                                 className="w-20 h-20 object-cover rounded-lg"
                                             />
                                             <h3 className="font-medium text-start">
-                                                {wishlist.title}
+                                                {product.title}
                                             </h3>
                                         </td>
-                                        <td>${wishlist.unitPrice}</td>
-                                        <td>{wishlist.stock}</td>
+                                        <td>${product.price}</td>
                                         <td>
-                                            <Button>Add to Cart</Button>
+                                            {Number(product.quantity) > 0
+                                                ? "In Stock"
+                                                : "Out of Stock"}
+                                        </td>
+                                        <td>
+                                            <Button onClick={() => handleAddToCart(product._id)}>
+                                                Add to Cart
+                                            </Button>
                                         </td>
                                     </tr>
                                 ))}
@@ -82,7 +92,7 @@ const WishlistPage = () => {
                     <div className="py-14 text-end flex flex-col items-end space-y-3">
                         <p className="text-lg text-grey-600">Estimated Total:</p>
                         <h2 className="text-2xl font-bold">
-                            ${Math.round(totalPrice * 100) / 100}
+                            ${Math.round(Number(TOTAL_PRICE) * 100) / 100}
                         </h2>
                         <Button className="py-4 px-28">Add All to Cart</Button>
                     </div>
