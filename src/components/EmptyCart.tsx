c327a1dd4604c43119b7691ca1426b4d6e9d870a
Obasi Chizitere <blossomobasi2@gmail.com>
2024-09-26 11:21:32 +0100

    Style: Reduce text size on mobile

--- src/components/EmptyCart.tsx
+++ src/components/EmptyCart.tsx
@@ -1,7 +1,9 @@
 const EmptyCart = () => {
     return (
-        <span className="flex flex-col items-center justify-center space-y-2 w-full py-20">
-            <h2 className="text-5xl font-bold nichrome">There is no Item in your cart :(</h2>
+        <span className="flex flex-col items-center justify-center text-center space-y-2 w-full py-20">
+            <h2 className="sm:text-5xl text-4xl font-bold nichrome">
+                There is no Item in your cart :(
+            </h2>
             <p className="text-grey-600">Make your cart happy by adding an item :)</p>
         </span>
     );
