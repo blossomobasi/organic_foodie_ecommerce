3d9278f721820cb29b967aff23eea043949da3a5
Obasi Chizitere <blossomobasi2@gmail.com>
2024-09-28 08:58:09 +0100

    Update: Navigate to the home page on success

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -2,14 +2,17 @@ import { useMutation } from "@tanstack/react-query";
 import { placeOrder as placeOrderApi } from "../services";
 import { toast } from "react-toastify";
 import { AxiosError } from "axios";
+import { useNavigate } from "react-router-dom";
 
 export const useOrder = () => {
+    const navigate = useNavigate();
     const { mutate: placeOrder, isPending: isPlacingOrder } = useMutation({
         mutationFn: placeOrderApi,
         onSuccess: (data) => {
             toast.success(data.message);
             // Redirect to the payment page in a blank tab
             window.open(data.authorization_url, "_blank");
+            navigate("/");
         },
         onError: (err: AxiosError) => {
             const errorMessage = (err.response?.data as { message: string }).message;
