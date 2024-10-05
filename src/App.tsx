73c9b43d5ca945c0069f513aa7b4e7743042f7ae
Obasi Chizitere <blossomobasi2@gmail.com>
2024-10-05 15:02:56 +0100

    Update: add otp page

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import SignupPage from "./pages/SignupPage";
 import { ToastContainer } from "react-toastify";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 import ResetPasswordPage from "./pages/ResetPasswordPage";
+import OTPPage from "./pages/OTPPage";
 
 function App() {
     const queryClient = new QueryClient({
@@ -52,6 +53,7 @@ function App() {
                     <Route path="login" element={<LoginPage />} />
                     <Route path="signup" element={<SignupPage />} />
                     <Route path="forgot-password" element={<ForgotPasswordPage />} />
+                    <Route path="OTP" element={<OTPPage />} />
                     <Route path="reset-password" element={<ResetPasswordPage />} />
 
                     <Route path="*" element={<NotFoundPage />} />
