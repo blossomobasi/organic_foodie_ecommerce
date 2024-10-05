a1987a9db6013687baee869da78ebb7afffd9699
Obasi Chizitere <blossomobasi2@gmail.com>
2024-10-05 15:07:04 +0100

    Update paramete to reset password

--- src/pages/ResetPasswordPage.tsx
+++ src/pages/ResetPasswordPage.tsx
@@ -1,24 +1,28 @@
-import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { useMutation } from "@tanstack/react-query";
-import { updatePassword as updatePasswordApi } from "../services";
+import { resetPassword as resetPasswordApi } from "../services";
 import { AxiosError } from "axios";
 import { toast } from "react-toastify";
+import { useState } from "react";
 
 import AuthLayout from "../ui/AuthLayout";
 import TextInput from "../ui/TextInput";
 import Button from "../ui/Button";
 
 const ResetPasswordPage = () => {
-    const userId = Cookies.get("userId") || "";
+    const [showPassword, setShowPassword] = useState(false);
+    const email = localStorage.getItem("email") || "";
     const navigate = useNavigate();
-    const { register, handleSubmit, formState } = useForm<{ password: string }>();
+    const { register, handleSubmit, formState, getValues } = useForm<{
+        password: string;
+        confirmPassword: string;
+    }>();
     const { errors } = formState;
 
     const { mutate: resetPassword, isPending } = useMutation({
-        mutationFn: ({ password, userId }: { password: string; userId: string }) =>
-            updatePasswordApi(password, userId),
+        mutationFn: ({ email, password }: { email: string; password: string }) =>
+            resetPasswordApi(email, password),
         onSuccess: () => {
             toast.success("Password updated successfully");
             navigate("/login");
@@ -32,7 +36,7 @@ const ResetPasswordPage = () => {
     const onSubmit = (data: { password: string }) => {
         if (isPending) return;
 
-        resetPassword({ password: data.password, userId });
+        resetPassword({ password: data.password, email });
     };
 
     return (
@@ -47,11 +51,28 @@ const ResetPasswordPage = () => {
 
                 <form className="flex flex-col space-y-3" onSubmit={handleSubmit(onSubmit)}>
                     <TextInput
+                        type={showPassword ? "text" : "password"}
+                        onIconClick={() => setShowPassword((showPassword) => !showPassword)}
+                        showPassword={showPassword}
                         placeholder="New Password"
                         {...register("password", {
                             required: "Password is required",
                         })}
+                        authInput
                         error={errors.password?.message}
+                    />
+                    <TextInput
+                        type={showPassword ? "text" : "password"}
+                        onIconClick={() => setShowPassword((showPassword) => !showPassword)}
+                        showPassword={showPassword}
+                        placeholder="Confirm Password"
+                        {...register("confirmPassword", {
+                            required: "Confirm Password is required",
+                            validate: (value) =>
+                                value === getValues("password") || "Passwords do not match",
+                        })}
+                        authInput
+                        error={errors.confirmPassword?.message}
                     />
                     <Button isLoading={isPending}>Submit</Button>
                 </form>
